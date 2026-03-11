b2d0e7d3ce4943b5aefb7ccdcb9971f2e333e847
myelectricpants <joshkimmel@gmail.com>
2026-03-11 17:56:40 -0400

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -332,7 +332,7 @@ module.exports = function (eleventyConfig) {
     // You can also pass this in on the command line using `--pathprefix`
 
     // Optional (default is shown)
-    pathPrefix: "/",
+    pathPrefix: "/digitalash/",
     // -----------------------------------------------------------------
 
     // These are all optional (defaults are shown):
